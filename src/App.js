a276f9d9a9c26556c026774bd3957212ace9732f
ashi72 <32313598+ashi72@users.noreply.github.com>
2022-10-01 16:00:31 -0400

    initial commit of just a restaurant card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
+import './App.css'
+import RestaurantCard from './components/RestaurantCard';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RestaurantCard name = "Jeff's Jelly Beans" location = "199 De Naur Drive"/>
     </div>
   );
 }
---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Card, CardContent, Typography } from '@mui/material/';
+import { styled } from '@mui/material/styles';
+
+const PREFIX = 'RCard';
+const classes = {
+  root: `${PREFIX}-root`,
+  nameText: `${PREFIX}-name-text`,
+  locationText: `${PREFIX}-location-text`,
+}
+const Root = styled('div')(({ theme }) => ({
+  [`&.${classes.root}`]: {
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: theme.palette.primary.main
+  },
+  [`& .${classes.nameText}`]: {
+    fontSize: '16px',
+    marginBottom: '1%',
+    fontWeight: 'bolder',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    color: '#000000',
+  },
+  [`& .${classes.locationText}`]: {
+    fontSize: '15px',
+    fontWeight: 'normal',
+    color: '#4F4F4F',
+  },
+}))
+
+export default function RestaurantCard({ name, location }) {
+    return (
+    <Root className={classes.root}>
+        <Card variant="outlined" className={classes.cardContainer}>
+            <CardContent>
+                <Typography className={classes.nameText}>
+                {name}
+                </Typography>
+                <Typography className={classes.locationText}>
+                {location}
+                </Typography>
+            </CardContent>
+        </Card>
+    </Root>
+    );
+  }
